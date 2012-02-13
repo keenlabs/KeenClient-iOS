232aedfe6fa394f593fd6b4814aa3dac95888cd0
Daniel Kador <dkador@gmail.com>
2012-02-12 18:53:02 -0600

    add KeenConstants

---
+++ KeenClient/KeenConstants.h
@@ -0,0 +1,12 @@
+//
+//  KeenConstants.h
+//  KeenClient
+//
+//  Created by Daniel Kador on 2/12/12.
+//  Copyright (c) 2012 Keen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const KeenServerAddress;
+extern NSString * const KeenApiVersion;
---
+++ KeenClient/KeenConstants.m
@@ -0,0 +1,12 @@
+//
+//  KeenConstants.m
+//  KeenClient
+//
+//  Created by Daniel Kador on 2/12/12.
+//  Copyright (c) 2012 Keen Labs. All rights reserved.
+//
+
+#import "KeenConstants.h"
+
+NSString * const KeenServerAddress = @"http://api.keen.io";
+NSString * const KeenApiVersion = @"1.0";
