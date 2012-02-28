b774db49f4ab392ad7633a3ff649a0eefbd05419
Daniel Kador <dkador@gmail.com>
2012-02-28 17:03:33 -0600

    clean up example project

--- KeenClientExample/KeenClientExample/AppDelegate.m
+++ KeenClientExample/KeenClientExample/AppDelegate.m
@@ -53,12 +53,13 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
+    NSLog(@"something");
     
     UIBackgroundTaskIdentifier taskId = [application beginBackgroundTaskWithExpirationHandler:^(void) {
         NSLog(@"Background task is being expired.");
     }];
     
-    [[KeenClient lastRequestedClient] uploadWithFinishedBlock:^(void) {
+    [[KeenClient sharedClient] uploadWithFinishedBlock:^(void) {
         [application endBackgroundTask:taskId];
     }];
 }
@@ -75,7 +76,8 @@
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-    [KeenClient clientForProject:@"4f4335253ba3017246000000" andAuthToken:@"320f97b19c8148ee9e84a0d27e2d0b85"];
+    [KeenClient sharedClientWithProjectId:@"4f4335253ba3017246000000" 
+                             andAuthToken:@"320f97b19c8148ee9e84a0d27e2d0b85"];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
