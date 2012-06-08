1008b5da2b9573b579a959db10a82c7943862ec1
Daniel Kador <dkador@gmail.com>
2012-06-08 11:11:02 -0700

    add script to generate docs and place them in the right path

---
+++ generate_docs.sh
@@ -0,0 +1,5 @@
+# generate docs
+appledoc --no-create-docset --project-name "Keen iOS Client" --project-company "Keen Labs" --company-id io.keen --output ~/help --index-desc docs/index.markdown KeenClient/KeenClient.h
+
+# copy correct files from output directory to Keen-Web
+cp -r ~/help/html/* ~/dev/keen/Keen-Web/app/static/docs/iOS-client/.
