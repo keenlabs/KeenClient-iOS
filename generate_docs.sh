f1fd08afe12ea3d60fe8eee11cc43c6c77159bf9
Daniel Kador <dkador@gmail.com>
2012-06-12 16:21:35 -0700

    make documentation generator list the files it's copying

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,4 +2,4 @@
 appledoc --no-create-docset --project-name "Keen iOS Client" --project-company "Keen Labs" --company-id io.keen --output ~/help --index-desc docs/index.markdown KeenClient/KeenClient.h
 
 # copy correct files from output directory to Keen-Web
-cp -r ~/help/html/* ~/dev/keen/Keen-Web/app/static/docs/iOS-client/.
+cp -v -r ~/help/html/* ~/dev/keen/Keen-Web/app/static/docs/iOS-client/.
