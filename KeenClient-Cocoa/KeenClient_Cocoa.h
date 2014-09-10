de597f0e63939b2319d5cb3fb3ad246d5a10b548
Terry Horner <terry.horner@gmailcom>
2014-09-10 15:53:53 -0700

    Added KeenClient-Cocoa target to support Mac OS X

---
+++ KeenClient-Cocoa/KeenClient_Cocoa.h
@@ -0,0 +1,13 @@
+//
+//  KeenClient_Cocoa.h
+//  KeenClient-Cocoa
+//
+//  Created by Terry Horner on 9/10/14.
+//  Copyright (c) 2014 Keen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeenClient_Cocoa : NSObject
+
+@end
---
+++ KeenClient-Cocoa/KeenClient_Cocoa.m
@@ -0,0 +1,13 @@
+//
+//  KeenClient_Cocoa.m
+//  KeenClient-Cocoa
+//
+//  Created by Terry Horner on 9/10/14.
+//  Copyright (c) 2014 Keen Labs. All rights reserved.
+//
+
+#import "KeenClient_Cocoa.h"
+
+@implementation KeenClient_Cocoa
+
+@end
