a9e11761e03bd8cdac8036613f00802d83c31ce9
claireyoung <clairez@gmail.com>
2015-05-04 19:22:31 -0700

    Add KeenSwiftClientExample project
    Add a simple Swift client example.

---
+++ KeenSwiftClientExample/KeenSwiftClientExample/KeenSwiftClientExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  KeenSwiftClientExample-Bridging-Header.h
+//  KeenSwiftClientExample
+//
+//  Created by Claire Young on 5/4/15.
+//  Copyright (c) 2015 Keen.IO. All rights reserved.
+//
+
+#ifndef KeenSwiftClientExample_KeenSwiftClientExample_Bridging_Header_h
+#define KeenSwiftClientExample_KeenSwiftClientExample_Bridging_Header_h
+
+#import "KeenClient.h"
+
+#endif
