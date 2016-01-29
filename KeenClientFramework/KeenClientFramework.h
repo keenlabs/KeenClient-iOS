4d4f2070ffd2763c3bb53cfb8e41673cc6cfcb1f
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2016-01-28 21:49:48 -0600

    Add Dynamic Framework target

---
+++ KeenClientFramework/KeenClientFramework.h
@@ -0,0 +1,27 @@
+//
+//  KeenClientFramework.h
+//  KeenClientFramework
+//
+//  Created by Heitor Sergent on 1/27/16.
+//  Copyright © 2016 Keen Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeenClientFramework.
+FOUNDATION_EXPORT double KeenClientFrameworkVersionNumber;
+
+//! Project version string for KeenClientFramework.
+FOUNDATION_EXPORT const unsigned char KeenClientFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeenClientFramework/PublicHeader.h>
+
+#import <KeenClient/KeenClient.h>
+#import <KeenClient/KeenProperties.h>
+#import <KeenClient/KeenProperties.h>
+
+#import <KeenClient/KIODBStore.h>
+#import <KeenClient/KIOQuery.h>
+#import <KeenClient/KIOReachability.h>
+
+#import <KeenClient/HTTPCodes.h>
