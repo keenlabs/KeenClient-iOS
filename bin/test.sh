a3551453d3260665db15c18f5fb67b80a81a8643
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2016-02-05 14:35:33 -0600

    Create test shell script and update travis.yml

---
+++ bin/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+xcodebuild \
+	-scheme KeenClient \
+	-sdk iphonesimulator \
+	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
+  ONLY_ACTIVE_ARCH=NO \
+	clean test | xcpretty --color
