0bddf97451d766c13c87143336322d6183d7d3ea
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2016-03-17 14:42:38 -0500

    Enable code coverage flag for test script
    Change flag for test script

--- bin/test.sh
+++ bin/test.sh
@@ -5,5 +5,6 @@ xcodebuild \
 	-scheme KeenClient \
 	-sdk iphonesimulator \
 	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
+	-enableCodeCoverage YES \
   ONLY_ACTIVE_ARCH=NO \
 	clean test | xcpretty --color
