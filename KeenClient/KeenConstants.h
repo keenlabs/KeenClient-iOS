5dc1c7c0dd8862642ea89a50dd6ee23ad92c4bff
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2016-06-28 10:16:35 -0500

    3.5.5 release bits

--- KeenClient/KeenConstants.h
+++ KeenClient/KeenConstants.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define kKeenSdkVersion @"3.5.4"
+#define kKeenSdkVersion @"3.5.5"
 
 extern NSString * const kKeenServerAddress;
 extern NSString * const kKeenApiVersion;
