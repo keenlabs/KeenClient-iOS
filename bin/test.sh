729b39a626342a1a140ffcaf3ab78ca31c89299a
Brian Baumhover <b.baumhover@gmail.com>
2017-03-22 14:33:42 -0500

    Backpedal on build then test change, since it increases build time. Instead build sample apps before building and testing KeenClient. -enableCodeCoverage is redundant since it's enabled in the KeenClient scheme, so remove it.

--- bin/test.sh
+++ bin/test.sh
@@ -2,9 +2,22 @@
 set -e -o pipefail
 
 xcodebuild \
+	-scheme KeenSwiftClientExample \
+	-sdk iphonesimulator \
+	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
+  ONLY_ACTIVE_ARCH=NO \
+	clean build | bundle exec xcpretty --color
+
+xcodebuild \
+	-scheme KeenClientExample \
+	-sdk iphonesimulator \
+	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
+  ONLY_ACTIVE_ARCH=NO \
+	clean build | bundle exec xcpretty --color
+
+xcodebuild \
 	-scheme KeenClient \
 	-sdk iphonesimulator \
 	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
-	-enableCodeCoverage YES \
   ONLY_ACTIVE_ARCH=NO \
-	test | bundle exec xcpretty --color
+	clean test | bundle exec xcpretty --color
