95a32b3b9573fe79171b24ea557cf35920037b07
Brian Baumhover <b.baumhover@gmail.com>
2017-03-22 13:36:32 -0500

    Build, then test.

--- bin/test.sh
+++ bin/test.sh
@@ -7,18 +7,4 @@ xcodebuild \
 	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
 	-enableCodeCoverage YES \
   ONLY_ACTIVE_ARCH=NO \
-	clean test | bundle exec xcpretty --color
-
-xcodebuild \
-	-scheme KeenSwiftClientExample \
-	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
-  ONLY_ACTIVE_ARCH=NO \
-	clean build | bundle exec xcpretty --color
-
-xcodebuild \
-	-scheme KeenClientExample \
-	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
-  ONLY_ACTIVE_ARCH=NO \
-	clean build | bundle exec xcpretty --color
+	test | bundle exec xcpretty --color
