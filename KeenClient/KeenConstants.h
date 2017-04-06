9df83577d193e2fda6c0ead628e8ae1d5104efce
Brian Baumhover <b.baumhover@gmail.com>
2017-04-05 23:42:10 -0500

    3.6.0 release

--- KeenClient/KeenConstants.h
+++ KeenClient/KeenConstants.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#define kKeenSdkVersion @"3.5.7"
+#define kKeenSdkVersion @"3.6.0"
 
 extern NSString * const kKeenServerAddress;
 extern NSString * const kKeenApiVersion;
