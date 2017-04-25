16c605642e6d9d9eb0bf27f1199a1b4425b5033e
Brian Baumhover <b.baumhover@gmail.com>
2017-04-25 16:10:38 -0500

    Deploy tests on an iOS 10.3 simulator.

--- bin/test.sh
+++ bin/test.sh
@@ -4,27 +4,24 @@ set -e -o pipefail
 xcodebuild \
 	-scheme KeenSwiftClientExample \
 	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
   ONLY_ACTIVE_ARCH=NO \
 	clean build | bundle exec xcpretty --color
 
 xcodebuild \
 	-scheme KeenClientExample \
 	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
   ONLY_ACTIVE_ARCH=NO \
 	clean build | bundle exec xcpretty --color
 
 xcodebuild \
 	-scheme KeenClientFramework \
 	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
   ONLY_ACTIVE_ARCH=NO \
 	clean build | bundle exec xcpretty --color
 
 xcodebuild \
 	-scheme KeenClient \
 	-sdk iphonesimulator \
-	-destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' \
+	-destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3' \
   ONLY_ACTIVE_ARCH=NO \
 	clean test | bundle exec xcpretty --color
