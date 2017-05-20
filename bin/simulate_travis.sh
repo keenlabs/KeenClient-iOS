dba3b46eeca0ed53f11ca922b65f684baf85217f
Brian Baumhover <b.baumhover@gmail.com>
2017-05-20 16:21:56 -0500

    Update ObjC project to build frameworks for pods, update simulate_travis.sh

--- bin/simulate_travis.sh
+++ bin/simulate_travis.sh
@@ -49,12 +49,15 @@ export POD_INSTALL=false
 export CARTHAGE_INSTALL=true
 $BUILD_SCRIPT
 
+export XCODEBUILD_PROJECT=
+export XCODEBUILD_PROJECT_TARGET=
 export XCODEBUILD_WORKSPACE=Examples/KeenClientExampleSwift/KeenClientExampleSwiftCocoaPods/KeenClientExampleSwiftCocoaPods.xcworkspace
 export TRAVIS_XCODE_SCHEME=KeenClientExampleSwiftCocoaPods
 export TRAVIS_XCODE_SDK=iphonesimulator10.3
 export XCODEBUILD_PLATFORM='iOS Simulator'
 export XCODEBUILD_ACTION=build
 export POD_INSTALL=true
+export CARTHAGE_INSTALL=false
 $BUILD_SCRIPT
 
 export XCODEBUILD_PROJECT=Examples/KeenClientExampleSwift/KeenClientExampleSwiftCarthage/KeenClientExampleSwiftCarthage.xcodeproj
